84212dbf6d72b45d6f283a28c9ecb2021a51384a
进 黎 <1362040599@qq.com>
2021-02-03 17:13:43 +0800

    新增默认加载界面

--- MAT_Shopping/Home.Master.designer.cs
+++ MAT_Shopping/Home.Master.designer.cs
@@ -49,5 +49,14 @@ namespace MAT_Shopping
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+
+        /// <summary>
+        /// ContentPlaceHolder3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder3;
     }
 }
